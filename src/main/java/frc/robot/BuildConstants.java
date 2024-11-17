3c0a974974177323d3e16db8f41bced58b653e6f
Andrew Durstewitz <adurstewitz@gmail.com>
2024-11-17 16:51:02 -0500

    Versioning files

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "SPORK_Core";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = -1;
-  public static final String GIT_SHA = "UNKNOWN";
-  public static final String GIT_DATE = "UNKNOWN";
-  public static final String GIT_BRANCH = "UNKNOWN";
-  public static final String BUILD_DATE = "2024-11-17 16:44:46 EST";
-  public static final long BUILD_UNIX_TIME = 1731879886253L;
-  public static final int DIRTY = 129;
+  public static final int GIT_REVISION = 1;
+  public static final String GIT_SHA = "aac8721f4190259340eba8286ae7de3bec9afae0";
+  public static final String GIT_DATE = "2024-11-17 16:47:01 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2024-11-17 16:47:44 EST";
+  public static final long BUILD_UNIX_TIME = 1731880064777L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
