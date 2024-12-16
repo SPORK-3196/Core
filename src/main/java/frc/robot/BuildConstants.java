cc47a28664741407e8c534f6bcd4a29b0d0f249b
Andrew Durstewitz <adurstewitz@gmail.com>
2024-12-16 14:59:51 -0500

    Adding AdvantageScope Simulation files

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "SPORK_Core";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 11;
-    public static final String GIT_SHA = "8c4dfaa5b14d41cd63cefa9c3366238d77c6747b";
-    public static final String GIT_DATE = "2024-12-10 19:30:34 EST";
+    public static final int GIT_REVISION = 12;
+    public static final String GIT_SHA = "c0e9de98ecec0c657b02365ef00dfabdbf824962";
+    public static final String GIT_DATE = "2024-12-16 14:54:29 EST";
     public static final String GIT_BRANCH = "main";
-    public static final String BUILD_DATE = "2024-12-16 14:53:54 EST";
-    public static final long BUILD_UNIX_TIME = 1734378834809L;
+    public static final String BUILD_DATE = "2024-12-16 14:59:12 EST";
+    public static final long BUILD_UNIX_TIME = 1734379152149L;
     public static final int DIRTY = 1;
 
     private BuildConstants() {}
