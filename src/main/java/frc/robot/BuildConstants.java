e59c2b22f1c4c568c9c3844a5828a9e4221a9759
Andrew Durstewitz <adurstewitz@gmail.com>
2025-01-03 15:09:54 -0500

    Updated compatibility to MapleSim 0.2.8

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "SPORK_Core";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 13;
-    public static final String GIT_SHA = "cc47a28664741407e8c534f6bcd4a29b0d0f249b";
-    public static final String GIT_DATE = "2024-12-16 14:59:51 EST";
+    public static final int GIT_REVISION = 14;
+    public static final String GIT_SHA = "a5f56c8203eff296158cdb7e0d374bfeee2faefd";
+    public static final String GIT_DATE = "2024-12-16 17:33:31 EST";
     public static final String GIT_BRANCH = "main";
-    public static final String BUILD_DATE = "2024-12-16 17:27:09 EST";
-    public static final long BUILD_UNIX_TIME = 1734388029631L;
+    public static final String BUILD_DATE = "2025-01-03 15:09:40 EST";
+    public static final long BUILD_UNIX_TIME = 1735934980783L;
     public static final int DIRTY = 1;
 
     private BuildConstants() {}
